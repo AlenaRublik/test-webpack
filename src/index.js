7ca5b4483d4d984ada79f7e15b05e23ac436da86
AlenaRublik <114560252+AlenaRublik@users.noreply.github.com>
2023-03-12 17:43:11 +0200

    add config webpack

--- src/index.js
+++
@@ -1,3 +0,0 @@
-import name from './module-1';
-
-console.log(name);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,32 @@
 const path = require('path');
+const HtmlMinimizerPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'my-bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlMinimizerPlugin({ template: 'src/index.html' }),
+        new MiniCssExtractPlugin({ filename: 'styles.css', }),
+    ],
+    devServer: {
+        port: 4444,
+        open: true,
     },
 };
